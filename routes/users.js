582f914123ddf8fb290d0ebe33ecf52049658cf7
NeutronStar00 <anshul1202singh@gmail.com>
2023-12-11 01:37:51 +0530

    added mongoose atlas url

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pin");
+mongoose.connect("mongodb+srv://anshul1202singh:NyLBea2NZhDg3gKQ@cluster0.xvgt87m.mongodb.net/");
 
 const userSchema = mongoose.Schema({
   email:{type:String, require:true, unique:true},
